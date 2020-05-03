11098c8e148382075ad1bec2cb7e9cc16177b73d
José da Silva Rodrigues <2989869+4thokage@users.noreply.github.com>
2020-05-03 03:08:25 +0100

    Add config file as os.Args

--- config/config.go
+++ config/config.go
@@ -13,9 +13,15 @@ type Config struct {
 	Database string
 }
 
-// Config Reads and parses the configuration file
+// Read and parse the configuration file
 func (c *Config) Read() {
-	if _, err := toml.DecodeFile("config.toml", &c); err != nil { // decode file config.toml
-		log.Fatal(err)
+	if len(os.Args) > 1 {
+		if _, err := toml.DecodeFile(os.Args[1], &c); err != nil { // try to decode argument file
+			log.Fatal(err)
+		}
+	} else {
+		if _, err := toml.DecodeFile("config.toml", &c); err != nil { //decode config file
+			log.Fatal(err)
+		}
 	}
 }
